bcfe1560bed6fdc96d942c1b0014a7bb2d9b45b1
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-01 21:50:31 -0700

    updated signup form with handlesubmit async await

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { FormLabel, FormInput, Button } from "react-native-elements";
+import axios from "axios";
+
+const ROOT_URL = "https://us-central1-one-time-password-app.cloudfunctions.net";
 
 class SignUpForm extends Component {
+  state = { phone: "" };
+
+  handleSubmit = async () => {
+    await axios.post(`${ROOT_URL}/createUser`, {
+      phone: this.state.phone
+    });
+    await axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+      phone: this.state.phone
+    });
+  };
+
   render() {
     return (
       <View>
-        <FormLabel>Enter Phone Number</FormLabel>
-        <FormInput />
-        <Button title="Submit" />
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput
+            value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
+        </View>
+        <Button onPress={this.handleSubmit} title="Submit" />
       </View>
     );
   }
