5240ddfb0371045ba59647901a764d6122d26aed
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-01 22:33:47 -0700

    incorporated signin form

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -5,19 +5,18 @@ import axios from "axios";
 
 const ROOT_URL = "https://us-central1-one-time-password-app.cloudfunctions.net";
 
-class SignUpForm extends Component {
-  state = { phone: "" };
+class SignInForm extends Component {
+  state = { phone: "", code: "" };
 
   handleSubmit = async () => {
+    const { phone, code } = this.state;
     try {
-      await axios.post(`${ROOT_URL}/createUser`, {
-        phone: this.state.phone
-      });
-      await axios.post(`${ROOT_URL}/requestOneTimePassword`, {
-        phone: this.state.phone
+      await axios.post(`${ROOT_URL}/verifyOneTimePassword`, {
+        phone,
+        code
       });
     } catch (err) {
-      console.log(err);
+      console.localStorage(err);
     }
   };
 
@@ -31,10 +30,19 @@ class SignUpForm extends Component {
             onChangeText={phone => this.setState({ phone })}
           />
         </View>
+
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Code</FormLabel>
+          <FormInput
+            value={this.state.code}
+            onChangeText={code => this.setState({ code })}
+          />
+        </View>
+
         <Button onPress={this.handleSubmit} title="Submit" />
       </View>
     );
   }
 }
 
-export default SignUpForm;
+export default SignInForm;
